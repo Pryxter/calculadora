552a0e9dce5a2b5df9fa69b06b1849d82ecb0909
Marcelo Hernandez <109430910+Pryxter@users.noreply.github.com>
2023-04-09 15:35:53 -0400

    Add files via upload

---
+++ src/components/Home/Footer.jsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="grid">
+          <div className="footer_container">
+            <h2>Calculator by Marcelo Hernandez</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
